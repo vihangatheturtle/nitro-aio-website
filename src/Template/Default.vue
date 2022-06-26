2ae029117d5d5752e6e6a11ae4c4c9cc33b1187c
Tolga Demirci <tolga.demirci@themarkagency.com.au>
2022-06-27 09:53:21 +1000

    fix page loader

--- src/Template/Default.vue
+++ src/Template/Default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-gradient-to-b from-black to-n-mBlue">
+  <div
+    class="bg-gradient-to-b from-black to-n-mBlue"
+    :class="{ 'h-screen': elementVisible }"
+  >
     <OrganismPageLoader />
     <a href="#content" class="sr-only text-blue-600 focus:not-sr-only">
       Skip to main content
@@ -12,3 +15,19 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      elementVisible: true,
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.elementVisible = false;
+    }, 2000);
+  },
+};
+</script>
